592debb3689b1915ab48bb1194d85415b55654f3
Gideon Peters <gideonpeters85@gmail.com>
2022-09-21 01:35:54 +0100

    feat: add binary search and two crystall balls implementation

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let start = 0;
+    let end = haystack.length;
 
+    while (start < end) {
+        const mid = Math.floor(start + (end - start) / 2);
+        const val = haystack[mid];
+
+        if (needle === val) {
+            return true;
+        } else if (needle < val) {
+            end = mid;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let end = breaks.length;
+    const jumpAmount = Math.floor(Math.pow(breaks.length, 1 / 2));
+    let i = jumpAmount;
 
+    for (; i < end; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+
+    for (let j = 0; j < jumpAmount && i < end; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
